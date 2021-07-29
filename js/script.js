33444d5337b86855250c35f99880b111176dd76e
leoloca98 <leonardolocatelli98@gmail.com>
2021-07-29 16:32:53 +0200

    Add tasks array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 /*
 
+#DESCRIZIONE
+Rifare l'esercizio della to do list come fatto assieme in classe:
+| - Stampare in pagina un item per ogni elemento contenuto in un array
+| - Ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+| - Predisporre un input per aggiungere un nuovo item alla lista: ciccando su un pulsante, il testo digitato viene aggiunto alla lista.
 
+*BONUS 1: Fare un controllo di validazione per non aggiungere degli item vuoti.
+*BONUS 2: Dedicarsi alla parte grafica aggiungendo un tocco personale e gestendo anche il mobile.
 
 */
+
+console.log('Vue is ready', Vue); //Lo uso solo per controllare se è davvero attivo
+
+Vue.config.devtools = true; //Attivare il tool di Vue sull'ispector
+
+const toDoList = new Vue({
+    el: '#toDoList',
+    data: {
+        tasks: [                            //Creo un array delle cose da fare (a cui toglieremo/aggiungeremo altre task)
+            'Portare fuori il cane',
+            'Andare a far la spesa',
+            'Cucinare per i fratelli',
+            'Finire gli esercizi',
+        ],
+    },
+    methods: {
+
+    },
+});
